de59e560849faaf58e876ce6640e750eafcec970
felizami <anuj.maharjan@gmail.com>
2016-12-06 21:38:46 +0545

    log4j2 issue

---
+++ src/main/java/com/anuz/dummyapi/config/Log4j2Config.java
@@ -0,0 +1,21 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package com.anuz.dummyapi.config;
+//
+//import java.io.File;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.core.LoggerContext;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// *
+// * @author anuz
+// */
+//@Configuration
+//public class Log4j2Config {
+//
+//   String contextPath= getServletContext().getRealPath("/");
+//}
